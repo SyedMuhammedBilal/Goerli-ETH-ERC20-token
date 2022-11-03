500f517cbd59bde22ad289b822abc1cff25fd3ec
SyedMuhammedBilal <intergo878@gmail.com>
2022-11-03 12:06:36 +0500

    erc-20 token

---
+++ ERC20-token/contracts/Outlast.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/security/Pausable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+contract Outlast is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("outlast", "OTT") {
+        _mint(msg.sender, 100e6 ether);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
